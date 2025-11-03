bd9897ccde2522312de2d4fbd3a6bdcae5a98211
red69ben <red69ben@gmail.com>
2025-11-03 19:27:43 +0200

    Delete src directory

--- src/App.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export default function App(){
-  return <div style={{color:'#fff',padding:24,fontFamily:'system-ui'}}>הפרויקט מוכן. הדבק כאן את הקוד המעודכן של App.tsx מהקנבס.</div>
-}
